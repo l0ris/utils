f8aa6b48dccc0112cf007ee1fe1c8beae02a5414
l0ris <l0ris@users.noreply.github.com>
2016-01-06 10:33:47 +0100

    Update bootstrap-puppet.sh

--- bootstrap-puppet.sh
+++ bootstrap-puppet.sh
@@ -6,14 +6,12 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-PUPPETMASTER="puppet.sysops.lab"
+PUPPETMASTER="puppet"
 
 # Installing Yum repo and puppet
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yum -y install puppet-server postgresql-server puppetdb puppetdb-terminus
 echo "Puppet Installed"
-# Initial puppet run!
-#puppet agent -tv --noop --server $PUPPETMASTER 
 
 # Install some puppet modules 
 sudo puppet module install -i /etc/puppet/environments/production/modules puppetlabs-ntp
